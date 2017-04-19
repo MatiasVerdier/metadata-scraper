7d2f62c4573003d223703f83b1c7be8cdf5d6dfc
Matias Verdier <verdier.matias@gmail.com>
2017-04-18 23:44:54 -0300

    Basic metadata scraper

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var Metascraper = require('metascraper');
 
 app.set('port', (process.env.PORT || 5000));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.get('/', function(request, response) {
-  response.end('Hola mundo');
+  response.end('Metadata scraper');
 });
 
+app.post('/', function(request, response) {
+  var url = request.body.url;
+  
+  Metascraper.scrapeUrl(url)
+    .then((metadata) => {
+      response.json(metadata);
+    })
+    .catch((error) => console.log(error));
+});
+  
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
